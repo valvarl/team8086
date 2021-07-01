344c1e69f1fd3c1ef17b8748034aed6f3a42f6ea
valvarl <valvarl@ya.ru>
2021-07-01 23:15:06 +0300

    Some design changes

--- src/NavBar/MenuItem.jsx
+++ src/NavBar/MenuItem.jsx
@@ -1,19 +1,29 @@
-import React, {useRef} from "react";
+import React, { useRef } from "react";
 import "./NavBar.css";
 import useHover from "./hooks/useHover";
 
 const MenuItem = (props) => {
-    const ref =useRef();
-    const isHovering = useHover(ref);
+  const ref = useRef();
+  const isHovering = useHover(ref);
 
   return (
-    <li ref={ref} className={isHovering ? 'menuitem active' : 'menuitem'}>
-      <div className={isHovering ? "navigation-icon-wrapper active" : 'navigation-icon-wrapper'}>
+    <li ref={ref} className={isHovering ? "menuitem active" : "menuitem"}>
+      <div
+        className={
+          isHovering
+            ? "navigation-icon-wrapper active"
+            : "navigation-icon-wrapper"
+        }
+      >
         <img className="navigation-icon" src={props.icon} alt="React Logo" />
       </div>
-        <div className='navigation-caption'>
-            {props.active ? <span>{props.caption}</span> : ''}
-        </div>
+      <div className="navigation-caption">
+        {props.active ? (
+          <span style={{ whiteSpace: "nowrap" }}>{props.caption}</span>
+        ) : (
+          ""
+        )}
+      </div>
     </li>
   );
 };
