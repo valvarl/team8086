e47177acf82b1b2813ad563761d1572568f73d03
valvarl <valvarl@ya.ru>
2021-07-08 18:04:17 +0300

    two menu items added, some front changes

--- src/NavBar/NavBar.jsx
+++ src/NavBar/NavBar.jsx
@@ -12,6 +12,8 @@ import { ReactComponent as formulaIcon } from "./img/formula.svg";
 import { ReactComponent as leafIcon } from "./img/leaf.svg";
 import { ReactComponent as scienceIcon } from "./img/science.svg";
 import { ReactComponent as disconnectIcon } from "./img/disconnect.svg";
+import { ReactComponent as flaskIcon } from "./img/flask.svg";
+import { ReactComponent as pipeIcon } from "./img/pipe.svg";
 
 const NavBar = () => {
   const [menuActive, setMenuActive] = useState(false);
@@ -19,11 +21,13 @@ const NavBar = () => {
     { id: 0, icon: factoryIcon, caption: "Главная", selected: true },
     { id: 1, icon: cctvIcon, caption: "Профиль", selected: false },
     { id: 2, icon: formulaIcon, caption: "Обучение", selected: false },
-    { id: 3, icon: dnaIcon, caption: "База знаний", selected: false },
     { id: 4, icon: chipIcon, caption: "Онлайн эмулятор", selected: false },
-    { id: 5, icon: scienceIcon, caption: "Форум", selected: false },
-    { id: 6, icon: leafIcon, caption: "О проекте", selected: false },
-    { id: 7, icon: disconnectIcon, caption: "Выйти", selected: false },
+    { id: 5, icon: dnaIcon, caption: "База знаний", selected: false },
+    { id: 6, icon: flaskIcon, caption: "Мои программы", selected: false },
+    { id: 7, icon: scienceIcon, caption: "Форум", selected: false },
+    { id: 8, icon: pipeIcon, caption: "Участники", selected: false },
+    { id: 9, icon: leafIcon, caption: "О проекте", selected: false },
+    { id: 10, icon: disconnectIcon, caption: "Выйти", selected: false },
   ]);
 
   function selectMenuItem(id) {
@@ -50,34 +54,36 @@ const NavBar = () => {
   });
 
   return (
-      <aside className={menuActive ? "nav-bar active menu" : "nav-bar menu"}>
-        <div className={menuActive ? "nav-bar active" : "nav-bar"}>
-          <div className="nav-scroller">
-            <div className="header">
-              {menuActive ? (
-                  <span className="logo-title">Team 8086</span>
-              ) : (
-                  <img className="logo" src={logo} alt="React logo" />
-              )}
-            </div>
+      <aside className={menuActive ? "menu drawerStart" : "menu drawerCollapsed"}>
+          <div className={menuActive ? "nav-bar drawerStart" : "nav-bar drawerCollapsed"}>
+            <div className={menuActive ? "nav-scroller drawerStart" : "nav-scroller drawerCollapsed"}>
+              <div className="header">
+                {menuActive ? (
+                    <span className="logo-title">Team 8086</span>
+                ) : (
+                    <img className="logo" src={logo} alt="React logo" />
+                )}
+              </div>
 
-            <ul className="nav-list">
-              {menuItems.slice(0, 1)}
-              <div className="divider" />
-              {menuItems.slice(1, 6)}
-              <div className="divider" />
-              {menuItems.slice(6)}
-            </ul>
-          </div>
-          <div
-              className="disclosure-button"
-              onClick={() => setMenuActive(!menuActive)}
-          >
+              <ul className="nav-list">
+                {menuItems.slice(0, 1)}
+                <div className="divider" />
+                {menuItems.slice(1, 6)}
+                <div className="divider" />
+                {menuItems.slice(6, 8)}
+                <div className="divider" />
+                {menuItems.slice(8)}
+              </ul>
+            </div>
+            <div
+                className="disclosure-button"
+                onClick={() => setMenuActive(!menuActive)}
+            >
         <span className="icon-wrapper material-icons">
           {menuActive ? "chevron_left" : "chevron_right"}
         </span>
+            </div>
           </div>
-        </div>
       </aside>
   );
 };
